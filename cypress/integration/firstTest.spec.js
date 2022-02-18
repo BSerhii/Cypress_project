15829eb4fa338baabf5d77ab218fc73304f0d79d
BSerhii <itsbakal@gmail.com>
2022-02-18 10:37:27 +0200

    using contains

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,2 +1,17 @@
 /// <reference types="Cypress"/>
 
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en');
+    cy.contains('div','Sign in');
+})
+
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en');
+    cy.contains('SIGN IN', {matchCase: false});
+})
+//
+it.only('', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en');
+    cy.get('footer').contains('Office')
+})
